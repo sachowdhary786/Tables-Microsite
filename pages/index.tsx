7e083467a536acaa6bce56adbbfb74552f169e27
Saad Chowdhary <sachowdhary786@gmail.com>
2022-12-22 19:34:14 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Page: NextPageWithLayout = () => {
     <Layout>
       <div className='pl-4 py-2 flex justify-between border-b-2 border-slate-200 flex-wrap items-center'>
         <h3 className='text-xl font-bold'>Bid Optimization</h3>
-<Link href='/ad-settings'> 
+<Link href='/ad-types'> 
         <button className='bg-apolloBlue text-white text-sm px-[1.3rem] py-[0.6rem] mx-4 my-1 rounded'>Add New Schedule</button>
       </Link>
 </div>
